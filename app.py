b7e8acbde78ab9455e041ef704dd34f21085953b
Victor <wanjala94vic@gmail.com>
2023-10-10 13:10:06 +0300

    adding labels

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
 
 
 class RegistrationApp(App):
@@ -10,7 +12,22 @@ class RegistrationApp(App):
         layout = BoxLayout(orientation = 'vertical', padding = 30, spacing = 10)
 
         head_label = Label(text='Python User Registration App', font_size=26,bold=True,height=40)
+        
+    
+        #Adding label
+        name_label = Label(text='Name:', font_size=18)
+        self.name_input = TextInput()
+        email_label = Label(text='Email:', font_size=18)
+        password_label = Label(text='Password:', font_size=18)
+        confirm_label = Label(text='Confirm password:', font_size=18)
+
+        layout.add_widget(head_label)
+        layout.add_widget(name_label)
+        layout.add_widget(email_label)
+        layout.add_widget(password_label)
+        layout.add_widget(confirm_label)
         return layout
+       
 
 if __name__=='__main__':
     RegistrationApp().run()
